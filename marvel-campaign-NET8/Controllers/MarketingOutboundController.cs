552109ee16ab61e96799a1ffafa18339aa40f5ed
vincent-epro <vincentwschu@eprotel.com.hk>
2025-05-02 17:46:08 +0800

    new API SearchOutboundCallList

--- marvel-campaign-NET8/Controllers/MarketingOutboundController.cs
+++ marvel-campaign-NET8/Controllers/MarketingOutboundController.cs
@@ -8,6 +8,9 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Linq.Dynamic.Core;
 using Z.EntityFramework.Plus;
+using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Globalization;
 
 namespace marvel_campaign_NET8.Controllers
 {
@@ -848,6 +851,309 @@ namespace marvel_campaign_NET8.Controllers
         }
 
 
+        // Search Outbound CallList
+        [Route("SearchOutboundCallList")]
+        [HttpPost]
+        public IActionResult SearchOutboundCallList([FromBody] JsonObject data)
+        {
+            string token = (data[AppInp.InputAuth_Token] ?? "").ToString();
+            string tk_agentId = (data[AppInp.InputAuth_Agent_Id] ?? "").ToString();
+
+            try
+            {
+                if (ValidateClass.Authenticated(token, tk_agentId))
+                {
+                    int batchNo = Convert.ToInt32((data["Batch_Id"] ?? "-1").ToString());
+
+                    if (batchNo == -1)
+                    {
+                        return Ok(new { result = AppOutp.OutputResult_FAIL, details = "Invalid Parameters." });
+                    }
+                    else
+                    {
+                        return Content(SearchCRM_OutboundCallList(data).ToString(), "application/json; charset=utf-8", Encoding.UTF8);
+                    }
+
+                }
+                else
+                {
+                    return Ok(new { result = AppOutp.OutputResult_FAIL, details = AppOutp.Not_Auth_Desc });
+                }
+
+            }
+            catch (Exception err)
+            {
+                return Ok(new { result = AppOutp.OutputResult_FAIL, details = err.Message });
+
+            }
+        }
+
+
+        private JObject SearchCRM_OutboundCallList(JsonObject data)
+        {
+            // declare a list of json objects containing the each row of data
+            List<JObject> _con_history_list = new List<JObject>();
+
+            // declare a json object to contain all rows of data
+            // JObject allJsonResults = new JObject(); //
+
+            int batch_id = Convert.ToInt32((data["Batch_Id"] ?? "-1").ToString());
+            int agent_id = Convert.ToInt32((data["To_Check_Id"] ?? "-1").ToString());
+
+            string phone = (data["Phone"] ?? "").ToString();
+            string name = (data["Name"] ?? "").ToString();
+            string gender = (data["Gender"] ?? "").ToString();
+
+            int age_from = Convert.ToInt32((data["Age_From"] ?? "-1").ToString());
+            int age_to = Convert.ToInt32((data["Age_To"] ?? "-1").ToString());
+
+            string call_status = (data["Call_Status"] ?? "").ToString();
+            string callback_time_from = (data["Callback_Time_From"] ?? "").ToString();
+            string callback_time_to = (data["Callback_Time_To"] ?? "").ToString();
+
+
+            List<string> conditionList = new List<string>();
+            string whereClause = string.Empty;
+
+            string statement = string.Empty;
+
+            statement = " c.Is_Valid == \"Y\"";
+
+            conditionList.Add("(" + statement + ")");
+
+            statement = " r.Opt_Out == null || !r.Opt_Out.Equals(\"Y\")";
+
+            conditionList.Add("(" + statement + ")");
+
+
+            if (batch_id != -1)
+            {
+                statement = " r.Batch_Id == " + batch_id;
+
+                conditionList.Add("(" + statement + ")");
+
+            }
+
+            if (agent_id != -1)
+            {
+                statement = " r.Agent_Id == " + agent_id;
+
+                conditionList.Add("(" + statement + ")");
+
+            }
+
+            if (phone != string.Empty)
+            {
+                statement = " c.Home_No.Contains(\"" + phone + "\")" + " || "
+                          + " c.Office_No.Contains(\"" + phone + "\")" + " || "
+                          + " c.Mobile_No.Contains(\"" + phone + "\")" + " || "
+                          + " c.Fax_No.Contains(\"" + phone + "\")" + " || "
+                          + " c.Other_Phone_No.Contains(\"" + phone + "\")";
+
+                conditionList.Add("(" + statement + ")");
+            }
+
+            if (name != string.Empty)
+            {
+                statement = " c.Name_Eng.Contains(\"" + name + "\")";
+
+                conditionList.Add("(" + statement + ")");
+            }
+
+            if (gender != string.Empty)
+            {
+                statement = " c.Gender.Equals(\"" + gender + "\")";
+
+                conditionList.Add("(" + statement + ")");
+            }
+
+            if (age_from != -1 && age_to != -1)
+            {
+                DateTime today = DateTime.Today;
+                DateTime min = today.AddYears(-(age_to + 1));
+                DateTime max = today.AddYears(-age_from);
+
+                int min_year = min.Year;
+                int min_month = min.Month;
+                int min_day = min.Day;
+
+                int max_year = max.Year;
+                int max_month = max.Month;
+                int max_day = max.Day;
+
+                statement = " c.DOB != null" + " && "
+                          + " c.DOB > " + "DateTime(" + min_year + "," + min_month + "," + min_day + ",0,0,0)" + " && "
+                          + " c.DOB <= " + "DateTime(" + max_year + "," + max_month + "," + max_day + ",0,0,0)";
+
+                conditionList.Add("(" + statement + ")");
+
+            }
+
+            if (call_status != string.Empty)
+            {
+                if (call_status == "NewLead")
+                    statement = " r.Attempt == 0";
+                else
+                    statement = " r.Call_Status.Equals(\"" + call_status + "\")";
+
+                conditionList.Add("(" + statement + ")");
+            }
+
+
+
+
+            if (callback_time_from != string.Empty && callback_time_to != string.Empty)
+            {
+                double s_min = TimeSpan.ParseExact(callback_time_from, @"hh\:mm", CultureInfo.InvariantCulture).TotalMinutes;
+                double e_min = TimeSpan.ParseExact(callback_time_to, @"hh\:mm", CultureInfo.InvariantCulture).TotalMinutes;
+
+                statement = "r.Callback_Time.HasValue "
+                          + " && ((r.Callback_Time.Value.Hour * 60) + r.Callback_Time.Value.Minute) >= " + s_min
+                          + " && ((r.Callback_Time.Value.Hour * 60) + r.Callback_Time.Value.Minute) <= " + e_min;
+
+
+                conditionList.Add("(" + statement + ")");
+
+            }
+
+            whereClause = string.Join(" AND ", conditionList.ToArray());
+
+
+            var _return_customer = (from r in _scrme.outbound_call_results
+                                    join c in _scrme.contact_lists on r.Customer_Id equals c.Customer_Id
+            select new { c, r }
+                   ).Where(whereClause);
+
+
+            int draw = Convert.ToInt32((data["draw"] ?? "-1").ToString());
+
+            var start = (data["start"] ?? "").ToString();
+            var length = (data["length"] ?? "").ToString();
+
+
+            int col_index = data["order"]?[0]?["column"]?.GetValue<int>() ?? -1;
+
+            string sortColumn = data["columns"]?[col_index]?["data"]?.GetValue<string>() ?? string.Empty;
+
+            var sortColumnDir = data["order"]?[0]?["dir"]?.GetValue<string>() ?? "asc";
+
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
+
+            //Sorting    
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+            {
+                string final_ordering;
+
+                bool isDesc;
+
+                if (sortColumnDir == "desc")
+                    isDesc = true;
+                else
+                    isDesc = false;
+
+                string sortColumn_list;
+
+                if (sortColumn == "Customer_Id" || sortColumn == "Name_Eng" || sortColumn == "Gender"
+                     || sortColumn == "Mobile_No" || sortColumn == "Email")
+                    sortColumn_list = "c.";
+                else
+                    sortColumn_list = "r.";
+
+
+                final_ordering = sortColumn_list + sortColumn + (isDesc ? " descending" : "");
+
+
+                // Sorting 2nd
+                if (data["order"]?.AsArray()?.Count > 1)
+                {
+                    int col_index2 = data["order"]?[1]?["column"]?.GetValue<int>() ?? -1;
+                    string sortColumn2 = data["columns"]?[col_index2]?["data"]?.GetValue<string>() ?? string.Empty;
+                    string sortColumnDir2 = data["order"]?[1]?["dir"]?.GetValue<string>() ?? "asc";
+
+                    string sortColumn_list2 = (sortColumn2 == "Customer_Id" || sortColumn2 == "Name_Eng" || sortColumn2 == "Gender"
+                                                || sortColumn2 == "Mobile_No" || sortColumn2 == "Email") ? "c." : "r.";
+
+                    final_ordering += $", {sortColumn_list2}{sortColumn2} {(sortColumnDir2 == "desc" ? "descending" : "")}";
+                }
+
+                _return_customer = _return_customer.OrderBy(final_ordering);
+            }
+
+
+            //total number of rows count     
+            recordsTotal = _return_customer.Count();
+            //Paging     
+            var data2 = _return_customer.Skip(skip).Take(pageSize).ToList();
+
+
+            // assign results' row values to the jsonResultList JSON object
+            foreach (var _result_item in data2)
+            {
+                // declare a temp json object to store each _result_item
+                JObject tempJson = new JObject();
+
+              //  tempJson.RemoveAll(); // clear the temp object
+
+                // iterate each column of the _result
+                foreach (PropertyInfo property in _result_item.c.GetType().GetProperties())
+                {
+                    switch (property.Name)
+                    {
+                        case "Photo": // skip adding Photo content to temp
+                            break;
+                        default:
+                            // For others, add the column name and value to temp
+                            tempJson.Add(new JProperty(property.Name, property.GetValue(_result_item.c)));
+                            break;
+                    }
+                }
+
+                foreach (PropertyInfo property in _result_item.r.GetType().GetProperties())
+                {
+                    // add only the following column names and values to temp
+                    switch (property.Name)
+                    {
+                        case "Customer_Id":
+                            break;
+
+                        case "Updated_By":
+                            // rename the column name and add value to temp
+                            tempJson.Add(new JProperty("Call_Updated_By", property.GetValue(_result_item.r)));
+                            break;
+
+                        case "Updated_Time":
+                            // rename the column name and add value to temp
+                            tempJson.Add(new JProperty("Call_Updated_Time", property.GetValue(_result_item.r)));
+                            break;
+
+                        default:
+                            // add the column name and value to temp
+                            tempJson.Add(new JProperty(property.Name, property.GetValue(_result_item.r)));
+                            break;
+
+                    }
+
+                }
+                _con_history_list.Add(tempJson); // add the temp result to the list
+            }
+
+            // set up _all_results json data
+            JObject allJsonResults = new JObject()
+            {
+                new JProperty("result", "success"),
+                new JProperty("draw", draw),
+                new JProperty("recordsFiltered", recordsTotal),
+                new JProperty("recordsTotal", recordsTotal),
+                new JProperty("details", _con_history_list)
+            };
+
+            // return all results in json format
+            return allJsonResults;
+
+        }
 
 
     }
